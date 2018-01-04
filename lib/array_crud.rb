2c6d734c1815823082f9896b0ae3397fba8d48d7
Helen Du <helendu.614@gmail.com>
2018-01-04 20:59:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,41 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = ["double", "dutch", "chocolate", "icecream"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["I", "love", "icecream"]
+  array.push("desserts!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["love", "icecream"]
+  array.unshift("I")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["love", "icecream", "desserts!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["I", "love", "icecream", "desserts!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["I", "love", "icecream", "desserts!"]
+  array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["I", "love", "icecream", "desserts!"]
+  array[1]
 end
 
 def retrieve_last_element_from_array(array)
-  
+
 end
